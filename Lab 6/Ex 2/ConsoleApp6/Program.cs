61806b6cfd79b87e1e8a648d292894a86e31fbde
IvanyukAndrew <73875988+IvanyukAndrew@users.noreply.github.com>
2021-04-12 08:16:18 +0300

    Add files via upload

---
+++ Lab 6/Ex 2/ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("c = ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введіть кількість елементів: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            int d = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = a[i-1]*a[i-1]+2*a[i-2];
+                if (a[i] > b && a[i] <= c)
+                {
+                    d += a[i];
+                }
+            }
+            int S = d / 2;
+            Console.Write(S);
+        }
+    }
+}
---
+++ Lab 6/Ex 4/Ex 4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ex_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            
+        }
+    }
+}
