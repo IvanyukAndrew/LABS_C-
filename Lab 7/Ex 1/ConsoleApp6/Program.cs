8c4890041a0de1ba09c1f6906e15939992599212
IvanyukAndrew <73875988+IvanyukAndrew@users.noreply.github.com>
2021-05-24 15:28:19 +0300

    Add files via upload

---
+++ Lab 7/Ex 1/ConsoleApp6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.Write("Ведіть n:");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            arr[n] = rand.Next(-10, 10);
+            Console.Write(n);
+
+        }
+    }
+}
---
+++ Lab 7/Ex 3/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a x1 = ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("a y1 = ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b x2 = ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b y2 = ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            int dob = x1 * x2 + y1 * y2;
+
+            if (dob == 0)
+            {
+                Console.WriteLine("Вектои є перпендикулярні");
+            }
+            else
+            {
+                Console.WriteLine("Вектои не є перпендикулярні");
+            }
+        }
+    }
+}
