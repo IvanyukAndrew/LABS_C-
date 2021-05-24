7fec5de5fb78d66c5c63892c5bb4903f3c515af1
IvanyukAndrew <73875988+IvanyukAndrew@users.noreply.github.com>
2021-05-24 15:29:03 +0300

    Add files via upload

---
+++ lab 8/Ex1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаб8_Завд1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            double y = Convert.ToDouble(textBox2.Text);
+            double z = Convert.ToDouble(textBox3.Text);
+            double upper = Funkcions.getmax(x, y, z) + Funkcions.getmax(x + y, x * y, 4 * z);
+            double lower = Funkcions.getmax(Math.Pow(Funkcions.getmax(x + y, x * y, x * x), 2), 7, z * z);
+            double answ = upper / lower;
+            textBox4.Text = answ.ToString();
+        }
+    }
+}
---
+++ lab 8/Ex3/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаб8_Завд1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x0 = 0;
+            int x2 = 9;
+            int x1 = 9;
+            Console.Write("n=");
+            int n = Convert.ToInt32(textBox1.Text);
+            int xn = 0;
+            for (int i = 3; i < n + 1; i++)
+            {
+                xn = x2 + 4 * x0;
+                x0 = x1;
+                x1 = x2;
+                x2 = xn;
+            }
+            textBox2.Text = xn.ToString();
+        }
+    }
+}
