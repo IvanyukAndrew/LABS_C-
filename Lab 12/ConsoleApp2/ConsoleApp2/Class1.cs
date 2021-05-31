58c65ea1960b7e55539d66f478bd473bf15227ca
IvanyukAndrew <73875988+IvanyukAndrew@users.noreply.github.com>
2021-05-31 22:16:18 +0300

    Add files via upload

---
+++ Lab 12/ConsoleApp2/ConsoleApp2/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class TMan
+    {
+        public string name;
+        public int age;
+        public string sex
+        {
+            get { return sex; }
+            set
+            {
+                if (value == "man" || value == "woman")
+                {
+                    sex = value;
+                }
+                else
+                {
+                    throw new Exception("da");
+                }
+            }
+        }
+
+        public TMan(string name, int age, string sex)
+        {
+            this.name = name;
+            this.age = age;
+            this.sex = sex;
+        }
+
+        public string getAgeState()
+        {
+            if (age > 24) return "adult";
+            else if (age > 16) return "teen";
+            else return "kid";
+        }
+    }
+}
